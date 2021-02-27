d940327e3694b6981089978d1b6e0df1f177946d
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-02-27 17:23:17 +0900

    2.8.1 PHP 템플릿

--- PHP-GET-Sanitized.php
+++ PHP-GET-Sanitized.php
@@ -1,4 +1,11 @@
 <?php
-$name = $_GET['name'];
+//$name = $_GET['name'];
 // htmlspecialchars() : 특별한 HTML 문자를 HTML 문자 엔티티로 변환한다.
-echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8').'님, 홈페이지 방문을 환영합니다!';
+//echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8').'님, 홈페이지 방문을 환영합니다!';
+
+$firstName = $_POST['firstname'];
+$lastName = $_POST['lastname'];
+// htmlspecialchars() : 특별한 HTML 문자를 HTML 문자 엔티티로 변환한다.
+echo htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8').' '.
+    htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8')
+    .'님, 홈페이지 방문을 환영합니다!';
---
+++ count.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <p>
+        <?php echo $output; ?>
+    </p>
+</body>
+</html>
